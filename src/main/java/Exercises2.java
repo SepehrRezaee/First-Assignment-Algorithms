b207bbf29fffdd61cbbf9682db535c6aac346a87
Sepehr Rezaee <78574247+SepehrRezaee@users.noreply.github.com>
2024-02-28 20:15:37 +0330

    Update Exercises2.java

--- src/main/java/Exercises2.java
+++ src/main/java/Exercises2.java
@@ -5,65 +5,71 @@ import java.util.Map;
 
 public class Exercises2 {
 
-    /*
-    Given an array of integers nums and an integer target, return indices of the two numbers
-    such that they add up to target.
-
-    You may assume that each input would have exactly one solution, and you may not use the same element twice.
-    You can return the answer in any order.
-    */
-
     public int[] twoSum(int[] nums, int target) {
-        // TODO
-        return null;
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            }
+            numMap.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
-
-    /*
-    Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
-
-    Symbol       Value
-    I             1
-    V             5
-    X             10
-    L             50
-    C             100
-    D             500
-    M             1000
-
-    For example, 2 is written as II in Roman numeral, just two ones added together.
-    12 is written as XII, which is simply X + II.
-    The number 27 is written as XXVII, which is XX + V + II.
-
-    Roman numerals are usually written largest to smallest from left to right.
-    However, the numeral for four is not IIII.
-    Instead, the number four is written as IV.
-    Because the one is before the five we subtract it making four.
-    The same principle applies to the number nine, which is written as IX.
-    There are six instances where subtraction is used:
-
-    I can be placed before V (5) and X (10) to make 4 and 9.
-    X can be placed before L (50) and C (100) to make 40 and 90.
-    C can be placed before D (500) and M (1000) to make 400 and 900.
-
-    Given a roman numeral, convert it to an integer.
-    */
 
     public int romanToInt(String s) {
-        // TODO
-        return 0;
+        Map<Character, Integer> romanMap = new HashMap<>() {{
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }};
+        int num = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i > 0 && romanMap.get(s.charAt(i)) > romanMap.get(s.charAt(i - 1))) {
+                num += romanMap.get(s.charAt(i)) - 2 * romanMap.get(s.charAt(i - 1));
+            } else {
+                num += romanMap.get(s.charAt(i));
+            }
+        }
+        return num;
     }
 
-    /*
-    Given an array nums of distinct integers, return all the possible permutations.
-    You can return the answer in any order.
-    */
-
     public List<List<Integer>> permute(int[] nums) {
-        // TODO
-        return null;
+        List<List<Integer>> list = new ArrayList<>();
+        backtrack(list, new ArrayList<>(), nums);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+        if(tempList.size() == nums.length){
+            list.add(new ArrayList<>(tempList));
+        } else{
+            for(int i = 0; i < nums.length; i++){ 
+                if(tempList.contains(nums[i])) continue; 
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
     }
 
     public static void main(String[] args) {
-        // test your code here!
+        Exercises2 exercises2 = new Exercises2();
+        int[] nums1 = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = exercises2.twoSum(nums1, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+
+        String roman = "MCMXCIV";
+        int num = exercises2.romanToInt(roman);
+        System.out.println("Integer: " + num);
+
+        int[] nums2 = {1, 2, 3};
+        List<List<Integer>> permute = exercises2.permute(nums2);
+        System.out.println("Permutations: " + permute);
     }
 }
